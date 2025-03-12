24129034d3ca67eb2a21ab23ef757c856a355a32
MammarDr <140546722+MammarDr@users.noreply.github.com>
2025-03-12 22:51:43 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -52,7 +52,7 @@ import {alertUser} from './utility.js';
 
       setTimeout( () => {
         window.location.hash = "";
-        window.location.href = window.location.href = `./URLShortner-PureJS/index.html`;
+        window.location.href = window.location.href = `./index.html`;
       }, 3000);
 
       return
